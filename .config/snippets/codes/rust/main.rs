f95fc31639349e55eb6b9ee8638f508cd407e054
Iwabuchi Yuki <iwabuchi@fun.bio.keio.ac.jp>
2023-01-18 01:15:30 +0900

    snippets added

---
+++ .config/snippets/codes/rust/main.rs
@@ -0,0 +1,13 @@
+/*
+ * FileName:     main
+ * CreatedDate:  2023-01-14 10:34:47 +0900
+ * LastModified: 2023-01-14 10:52:48 +0900
+ */
+
+
+use std::error;
+
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    Ok(())
+}
