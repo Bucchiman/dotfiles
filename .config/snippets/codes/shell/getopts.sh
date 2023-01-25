d9e64bc71951e92e0054190c6f25c76a8bd20a15
iwabuchi yuki <yukibuchi17.keio.jp>
2023-01-25 18:27:00 +0900

    snippets added

--- .config/snippets/codes/shell/getopts.sh
+++ .config/snippets/codes/shell/getopts.sh
@@ -2,7 +2,7 @@
 #
 # FileName: 	getopts
 # CreatedDate:  2023-01-05 17:55:19 +0900
-# LastModified: 2023-01-05 17:56:12 +0900
+# LastModified: 2023-01-25 15:51:12 +0900
 #
 
 function _usage() {
@@ -10,21 +10,32 @@ function _usage() {
     exit 1
 }
 
-while getopts :u:n:f: OPT
+if [[ $# = 0 ]]
+then
+    echo No arguments or options
+    exit 1
+fi
+
+while getopts :ab:c: OPT
 do
     case $OPT in
-        u) username=$OPTARG;;
-        n) image_name=$OPTARG;;
-        f) dockerfile=$OPTARG;;
+        a) flag=1;;
+        b) b=$OPTARG;;
+        c) c=$OPTARG;;
         :|\?) _usage;;
     esac
 done
+if [[ ! -z $flag ]]
+then
+    printf "stand flag\n"
+fi
 
 return
 
 #
 # a:bc:
 # -> aとcオプションは引数
+# -> command -a hoge -b -c geho
 # :a:bc:
 # -> 未定義オプションの場合、?として処理
 #
---
+++ .config/template/template.md
@@ -0,0 +1,11 @@
+<!-- FileName: <+FILENAME+>
+ Author: 8ucchiman
+ CreatedDate: <+DATE+>
+ LastModified: 2023-01-25 10:56:12 +0900
+ Reference: 8ucchiman.jp
+-->
+
+
+<+CURSOR+>
+
+
