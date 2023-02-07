189402367774e04ca2ce4aff961ad1a855c46721
iwabuchi yuki <yukibuchi17.keio.jp>
2023-02-07 18:34:25 +0900

    snippets added

--- .config/snippets/codes/rust/template/template.rs
+++ .config/snippets/codes/rust/template/template.rs
@@ -1,7 +1,7 @@
 /*
- * FileName:     main
+ * FileName:     template
  * CreatedDate:  2023-01-14 10:34:47 +0900
- * LastModified: 2023-01-14 10:52:48 +0900
+ * LastModified: 2023-02-07 12:54:36 +0900
  */
 
 
@@ -9,5 +9,6 @@ use std::error;
 
 
 fn main() -> Result<(), Box<dyn error::Error>> {
+    println!("Hello 8ucchiman!");
     Ok(())
 }
