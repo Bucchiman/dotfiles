92098e004a026f5be7bcdb095f4adc67dae6a847
Yuki Iwabuchi <yukibuchi17@keio.jp>
2023-02-26 13:46:37 +0900

    mylib added

--- .config/template/template.c
+++ .config/template/template.c
@@ -2,13 +2,16 @@
  * FileName:     <+FILENAME+>
  * Author: 8ucchiman
  * CreatedDate:  <+DATE+>
- * LastModified: 2023-02-21 11:29:04 +0900
+ * LastModified: 2023-02-26 13:30:39 +0900
  * Reference: 8ucchiman.jp
  */
 
 #include <stdio.h>
+#define MACRO
 
+#ifdef MACRO
 int main(int argc, char* argv[]){
     <+CURSOR+>
     return 0;
 }
+#endif
