e36b0e270821f10206f6fcdb3f5f74e8a2cb2cd2
iwabuchi yuki <yukibuchi17.keio.jp>
2023-03-27 11:40:51 +0900

    readme added

---
+++ .config/template/template.cu
@@ -0,0 +1,17 @@
+/*
+ * FileName:     <+FILENAME+>
+ * Author: 8ucchiman
+ * CreatedDate:  <+DATE+>
+ * LastModified: 2023-02-26 13:30:39 +0900
+ * Reference: 8ucchiman.jp
+ */
+
+#include <stdio.h>
+#define MACRO
+
+#ifdef MACRO
+int main(int argc, char* argv[]){
+    <+CURSOR+>
+    return 0;
+}
+#endif
