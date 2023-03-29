63a66bdd6e5ac7fdb156c1803c333f595c2d0a0b
Yuki Iwabuchi <yukibuchi17@keio.jp>
2023-03-29 23:42:17 +0900

    lua added

---
+++ .config/snippets/codes/lua/nvim/init.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/env lua
+--
+-- FileName:     init
+-- Author: 8ucchiman
+-- CreatedDate:  2023-03-26 11:40:26 +0900
+-- LastModified: 2023-03-26 13:59:00 +0900
+-- Reference: 8ucchiman.jp
+--
+
+
+vim.api.nvim_command('set runtimepath^=/home/bucchiman/.config/snippets/codes/lua/nvim')
+
+--require("")
