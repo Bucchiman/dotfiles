edfb3d648a7bf2bbe923d8749d0e887d86925971
Iwabuchi Yuki <yukibuchi17@keio.jp>
2023-03-30 15:12:36 +0900

    image8space added

--- .config/snippets/codes/c++/images/sift/main.cpp
+++ .config/snippets/codes/c++/images/sift/main.cpp
@@ -2,7 +2,7 @@
  * FileName:     main
  * Author: 8ucchiman
  * CreatedDate:  2023-03-28 11:43:11 +0900
- * LastModified: 2023-03-29 09:15:47 +0000
+ * LastModified: 2023-03-29 10:51:08 +0000
  * Reference: 8ucchiman.jp
  */
 
@@ -10,10 +10,10 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-//#include <opencv2/nonfree.hpp>
+#include <opencv2/nonfree.hpp>
 
 
 int main() {
-  cout << "Hello" << endl;
+  std::cout << "Hello" << std::endl;
   return 0;
 }
