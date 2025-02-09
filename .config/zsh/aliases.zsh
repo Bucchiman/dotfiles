afaeb43930a6e0d73be42c1e9a2ed0325d88c21f
8ucchiman <8ucchiman@gmail.com>
2025-02-09 16:24:13 +0900

    update submodule

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,7 +3,7 @@
 # FileName:     aliases
 # Author:       8ucchiman
 # CreatedDate:  2023-09-08 00:40:43
-# LastModified: 2024-02-09 23:52:04
+# LastModified: 2024-08-25 21:47:50
 # Reference:    https://github.com/xero/dotfiles/blob/main/zsh/.config/zsh/06-aliases.zsh
 # Description:  ---
 #
@@ -417,6 +417,7 @@ function 8ucchiman_aliases () {
 
     alias get_idf='. $HOME/source/esp/esp-idf/export.sh'
     alias arduino='arduino-cli'
+    alias 8tmp='ln -sf $HOME/dotfiles/.config/lib/codes/shell/pocket/Brun ./; cp $HOME/dotfiles/.config/lib/codes/shell/pocket/local ./;'
 }
 
 
