a238a3ed0875a4fb431dac3663ab81ced30dee87
8ucchiman <8ucchiman@gmail.com>
2025-03-22 13:22:27 +0900

    exclude onelines

--- windows/pocket/onelines/FULLPATH:(Get-Item $file_path ).FullName        ## C:\Users\Bucchiman\dotfiles\windows\Bmods.ps1
+++
@@ -1,5 +0,0 @@
-Command> (Get-Item $file_path ).FullName        ## C:\Users\Bucchiman\dotfiles\windows\Bmods.ps1
-
-Keywords> FULLPATH
-
-Reference> https://stackoverflow.com/questions/12503871/removing-path-and-extension-from-filename-in-powershell
